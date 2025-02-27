e6041f06725d6e32150fa12e6c78297394ad43b7
Trinh Nguyen <conggtrinh1061996@gmail.com>
2025-02-27 16:08:19 +0700

    Add necessary libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,18 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.com.google.devtools.ksp)
+    alias(libs.plugins.com.google.dagger.hilt.android)
 }
 
 android {
     namespace = "com.androidtech.base"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.androidtech.base"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -26,12 +28,35 @@ android {
             )
         }
     }
+
+    flavorDimensions += "default"
+    productFlavors {
+        create("dev") {
+            applicationId = "com.androiddev.androidprojects.dev"
+            resValue("string", "app_name", "Dev Android Projects")
+
+            buildConfigField("String", "DOMAIN_URL", "\"http://BaseURL/\"")
+            buildConfigField("String", "TOKEN", "\"Token\"")
+
+        }
+
+        create("prod") {
+            buildConfigField("String", "DOMAIN_URL", "\"http://BaseURL/\"")
+            buildConfigField("String", "TOKEN", "\"Token\"")
+        }
+    }
+
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "21"
+    }
+
+    buildFeatures {
+        viewBinding = true
+        buildConfig = true
     }
 }
 
@@ -45,4 +70,34 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // Timber
+    implementation(libs.com.jakewharton.timber)
+    // Moshi
+    implementation(libs.com.squareup.moshi.kotlin)
+    implementation(libs.com.squareup.retrofit2.converter.moshi)
+    // Gson
+    implementation(libs.com.google.code.gson)
+    // Coroutine
+    implementation(libs.org.jetbrains.kotlinx.coroutines)
+    // OkHttp
+    implementation(libs.com.squareup.okhttp)
+    implementation(libs.com.squareup.okhttp3.logging.interceptor)
+    // Room database
+    implementation(libs.androidx.runtime.room)
+    ksp(libs.androidx.room.compiler)
+    implementation (libs.androidx.room.ktx)
+
+    // life cycle
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.savedstate)
+
+    // Retrofit
+    implementation (libs.com.squareup.retrofit)
+    implementation (libs.com.squareup.retrofit.converter.gson)
+    // Skydoves
+    implementation (libs.com.github.skydoves.sandwich)
+    // Hilt
+    implementation(libs.dagger.hilt)
+    ksp(libs.dagger.hilt.android.compiler)
 }
