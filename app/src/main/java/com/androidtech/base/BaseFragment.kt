ecadca116855aa68c44dc27dff302143b59e7671
Trinh Nguyen <conggtrinh1061996@gmail.com>
2025-03-01 20:19:00 +0700

    Create BaseFragment class

---
+++ app/src/main/java/com/androidtech/base/BaseFragment.kt
@@ -0,0 +1,24 @@
+package com.androidtech.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseFragment<VB: ViewBinding>(
+    private val bindingInflater: (LayoutInflater) -> VB
+): Fragment() {
+
+    protected lateinit var binding: VB
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = bindingInflater.invoke(inflater)
+        return binding.root
+    }
+}
